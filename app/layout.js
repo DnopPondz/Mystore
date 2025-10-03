b28da372e8bf321c08390d2c43f25fe27f02ef7f
wachirodol Thongnate <166611378+DnopPondz@users.noreply.github.com>
2025-10-03 14:14:18 +0700

    Use Itim font across site

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="th">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Itim&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="min-h-screen text-[var(--color-text)] antialiased">
         {children}
       </body>
